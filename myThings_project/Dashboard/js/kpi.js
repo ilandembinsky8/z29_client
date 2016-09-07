4bc5ebcfae321303deb5bb88c4c9e489d34a7dc6
Ali Abu Arab <ali1_94@hotmail.com>
2016-09-07 14:10:48 +0300

    lastest v1.1

--- myThings_project/Dashboard/js/kpi.js
+++ myThings_project/Dashboard/js/kpi.js
@@ -1,17 +1,29 @@
-
 function onLoadPage(){
+	// Load select input values
+	// showData() func that api wrote it
+	var jsonData = JSON.parse(showData());
+	for(var i = 0; i < jsonData.length; i++) 
+     	$('#exchangeSelect').append($("<option></option>").attr("value",jsonData[i].id).text(jsonData[i].name));
+}
+
+
+
+
+
+
+/*function onLoadPage(){
 	
 	// Load select input values
 	$.get("getdata?func=getCampaigns", function(data){
 		var jsonData = JSON.parse(data);
         for(var i = 0; i < jsonData.length; i++) {
-        	$('#advertiserSelect').append($("<option></option>").attr("value",jsonData[i].id).text(jsonData[i].name)); 
+        	$('#exchangeSelect').append($("<option></option>").attr("value",jsonData[i].id).text(jsonData[i].name)); 
         }
     });
-}
+}*/
 
 
-$("#campaignSelect").change(function(){
+/*$("#campaignSelect").change(function(){
 	$("#multiSelectors").empty();
 	$("#multiSelectors").append("<div class='row'>"
 				+ "<div class='col-sm-3'>"
@@ -19,4 +31,4 @@ $("#campaignSelect").change(function(){
 				+ "<div class='col-sm-9'>"
 				+ "<select class='form-control' id='ad-groupSelect'>"
 	  			+ "<option>select</option></select></div>");
-});
+});*/
