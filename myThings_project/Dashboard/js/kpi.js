e2e542f26deb3bd688fea0253909ec01325db5e7
Ahmad Mnasra <Ahmad Mnasra>
2016-09-08 09:23:02 +0300

    update verison for last task v1.1.2

--- myThings_project/Dashboard/js/kpi.js
+++ myThings_project/Dashboard/js/kpi.js
@@ -1,34 +1,8 @@
-function showData(data){
-	// Load select input values
-	// showData() func that api wrote it
-	//var jsonData = JSON.parse(data);
-	for(var i = 0; i < data.length; i++) 
-     	$('#exchangeSelect').append($("<option></option>").text(jsonData[i].name));
-}
-
-
-
-
-
-
-/*function onLoadPage(){
-	
-	// Load select input values
-	$.get("getdata?func=getCampaigns", function(data){
-		var jsonData = JSON.parse(data);
-        for(var i = 0; i < jsonData.length; i++) {
-        	$('#exchangeSelect').append($("<option></option>").attr("value",jsonData[i].id).text(jsonData[i].name)); 
-        }
-    });
-}*/
-
-
-/*$("#campaignSelect").change(function(){
-	$("#multiSelectors").empty();
-	$("#multiSelectors").append("<div class='row'>"
-				+ "<div class='col-sm-3'>"
-				+ "<label>Ad Group</label></div>"
-				+ "<div class='col-sm-9'>"
-				+ "<select class='form-control' id='ad-groupSelect'>"
-	  			+ "<option>select</option></select></div>");
-});*/
+function showData(data){ 
+ 	// Load select input values 
+ 	var jsonData = JSON.parse(data.trim());
+	//data = data.trim();
+	alert(data);
+ 	for(var i = 0; i < jsonData.length; i++)  
+      	$('#exchangeSelect').append($("<option></option>").text(jsonData[i].data)); 
+ } 
